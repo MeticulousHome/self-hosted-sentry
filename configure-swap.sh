a8c4cd320b3a7b0b5fd757d436fd37cde348ae7f
Angel-Padilla <angel.p.lopez@outlook.com>
2025-10-13 17:53:45 -0600

    feat: add ssl to VPS
    An ngnix instance run in the VPS will hold the certificates for
    sentry.meticulousespresso.com and proxy_pass the requests to port
    8081.

--- configure-swap.sh
+++ configure-swap.sh
@@ -1,23 +1,24 @@
+#!/usr/bin/env bash
 set -euo pipefail
 
 # CSRF_TRUSTED_ORIGIN="http://65.109.232.162:9000"
-CSRF_TRUSTED_ORIGIN="http://sentry.meticulousespresso.com"
+CSRF_TRUSTED_ORIGIN="https://sentry.meticulousespresso.com"
 
 # modify generated configuration files
 
 ERROR=0
 if [ ! -e "./sentry/config.yml" ]; then
-    echo "missing file: ./sentry/config.yml"
-    ERROR=2
+  echo "missing file: ./sentry/config.yml"
+  ERROR=2
 fi
 
 if [ ! -e "./sentry/sentry.conf.py" ]; then
-    echo "don't run this script individually, run ./install.sh instead"
-    ERROR=2
+  echo "don't run this script individually, run ./install.sh instead"
+  ERROR=2
 fi
 
-if (( ERROR != 0 )); then
-    exit $ERROR
+if ((ERROR != 0)); then
+  exit $ERROR
 fi
 
 echo ""
@@ -28,9 +29,9 @@ echo ""
 echo " > Updating default kafka options"
 
 sed -i \
-    -e 's/\("message\.max\.bytes": \)[0-9]\+/\110000000/' \
-    -e 's/\("socket\.timeout\.ms": \)[0-9]\+/\160000/' \
-    ./sentry/sentry.conf.py
+  -e 's/\("message\.max\.bytes": \)[0-9]\+/\110000000/' \
+  -e 's/\("socket\.timeout\.ms": \)[0-9]\+/\160000/' \
+  ./sentry/sentry.conf.py
 
 echo ""
 echo -e "   -> message.max.bytes set to  100000000\n   -> socket.timeout.ms set to 60000"
@@ -50,6 +51,22 @@ echo ""
 sed -i -E "s|^#?[[:space:]]*system.url-prefix:.*|system.url-prefix: $CSRF_TRUSTED_ORIGIN|" ./sentry/config.yml
 
 echo -e "   -> system.url-prefix set to $CSRF_TRUSTED_ORIGIN"
+echo ""
+echo ""
+echo " > Configuring to work behind SSL reverse proxy"
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+sed -i -E "s|^#?[[:space:]]*SECURE_PROXY_SSL_HEADER =.*|SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')|" ./sentry/sentry.conf.py
+# USE_X_FORWARDED_HOST = True
+sed -i -E "s|^#?[[:space:]]*USE_X_FORWARDED_HOST =.*|USE_X_FORWARDED_HOST = True|" ./sentry/sentry.conf.py
+# SESSION_COOKIE_SECURE = True
+sed -i -E "s|^#?[[:space:]]*SESSION_COOKIE_SECURE =.*|SESSION_COOKIE_SECURE = True|" ./sentry/sentry.conf.py
+# CSRF_COOKIE_SECURE = True
+sed -i -E "s|^#?[[:space:]]*CSRF_COOKIE_SECURE =.*|CSRF_COOKIE_SECURE = True|" ./sentry/sentry.conf.py
+# SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
+sed -i -E "s|^#?[[:space:]]*SOCIAL_AUTH_REDIRECT_IS_HTTPS =.*|SOCIAL_AUTH_REDIRECT_IS_HTTPS = True|" ./sentry/sentry.conf.py
+
+echo ""
+echo ""
 
 echo " > Checking use of Swap space"
 echo ""
@@ -57,35 +74,34 @@ echo ""
 SWAP_CHECK="$(swapon --show)"
 
 if [ -n "$SWAP_CHECK" ]; then
-    echo "   -> swap space already configured"
-    echo "$SWAP_CHECK" | awk 'END{print}'
+  echo "   -> swap space already configured"
+  echo "$SWAP_CHECK" | awk 'END{print}'
 else
-    # set up swapspace
-    MINIMUM_SPACE_REQUIRED=4
+  # set up swapspace
+  MINIMUM_SPACE_REQUIRED=4
 
-    available_space=$(df --output=avail -BG / | tail -1 | sed 's/G//' | tr -d ' ')
+  available_space=$(df --output=avail -BG / | tail -1 | sed 's/G//' | tr -d ' ')
 
-    if (( available_space < MINIMUM_SPACE_REQUIRED )); then
-        echo " [x] Less than $MINIMUM_SPACE_REQUIRED G available, cannot set up swap space"
-        exit 1
-    fi
+  if ((available_space < MINIMUM_SPACE_REQUIRED)); then
+    echo " [x] Less than $MINIMUM_SPACE_REQUIRED G available, cannot set up swap space"
+    exit 1
+  fi
 
-    SWAP_SIZE="${MINIMUM_SPACE_REQUIRED}G"
-    fallocate -l "$SWAP_SIZE" /swapfile
-    chmod 600 /swapfile
-    mkswap /swapfile
-    swapon /swapfile
+  SWAP_SIZE="${MINIMUM_SPACE_REQUIRED}G"
+  fallocate -l "$SWAP_SIZE" /swapfile
+  chmod 600 /swapfile
+  mkswap /swapfile
+  swapon /swapfile
 
-    #save the swap config
-    echo "   -> Saving Swap space config"
-    echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+  #save the swap config
+  echo "   -> Saving Swap space config"
+  echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 fi
 
-
-  echo ""
-  echo "-----------------------------------------------------------------"
-  echo ""
-  echo "Now You're all set! Trust me :P"
-  echo ""
-  echo "-----------------------------------------------------------------"
-  echo ""
+echo ""
+echo "-----------------------------------------------------------------"
+echo ""
+echo "Now You're all set! Trust me :P"
+echo ""
+echo "-----------------------------------------------------------------"
+echo ""
